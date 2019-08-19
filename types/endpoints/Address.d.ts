ef669c500fbe124d694eaad443c800a597f9eb20
vinit <vinitm@nividata.com>
2019-08-19 15:47:53 +0530

    build

--- types/endpoints/Address.d.ts
+++ types/endpoints/Address.d.ts
@@ -2,5 +2,5 @@ import Http from '../Http';
 import { AddAddressAttr } from '../interfaces/Address';
 import { IToken, ITokenResult } from '../interfaces/Token';
 export default class Address extends Http {
-    public add(token: IToken, params: AddAddressAttr): Promise<ITokenResult>;
+    add(token: IToken, params: AddAddressAttr): Promise<ITokenResult>;
 }
